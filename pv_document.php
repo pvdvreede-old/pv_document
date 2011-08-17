3ce58306793cae8593098e4444d9e03c5f7cc76a
Paul Van de Vreede <paul@vdvreede.net>
2011-08-17 18:46:36 +1000

    initial commit.

---
+++ pv_document.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Document
+Plugin URI: http://todo.com
+Description: A brief description of the Plugin.
+Version: 0.5
+Author: Paul Van de Vreede
+Author URI: http://www.vdvreede.net
+License: A "Slug" license name e.g. GPL2
+*/
+
+
+
+
