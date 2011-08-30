43533bc8a0cf62eec5c62fe6c3c79d2baf4c1dcb
Paul Van de Vreede <paul@vdvreede.net>
2011-08-30 12:59:20 +1000

    Altered presentation of document content display.

--- pv_document.php
+++ pv_document.php
@@ -137,13 +137,16 @@ function pvd_format_content($content) {
         $file_path = get_post_meta($attachment->ID, '_wp_attached_file', true);      
         $middle_path = get_option('upload_path');
         $full_filename = ABSPATH . $middle_path . $file_path;         
-        $kb_size = filesize($full_filename) / 1024;
+        $byte_size = filesize($full_filename);
+        $kb_size = $byte_size;
+        
         
         $content .= '<div class="pv_document_item">';
+        $content .= '<hr />';
         $content .= '<img src="' . pvd_get_attachment_icon_url( $attachment->post_mime_type ) . '" />';
-        $content .= '<p>' . $attachment->post_name . '</p>';
-        $content .= '<p><a href="' . wp_get_attachment_url($attachment->ID) . '">Download</a></p>';
-        $content .= '<p>File size: ' . $kb_size . 'KB</p>';
+        $content .= '<p>' . basename($file_path) . '<br />';
+        $content .= '<a href="' . wp_get_attachment_url($attachment->ID) . '">Download</a><br />';
+        $content .= 'File size: ' . $kb_size . 'KB</p>';
         $content .= '</div>';
     }
     
